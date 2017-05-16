2459b2d1e22c35101c3b495550f33afa0689dfd6
Lizette Romero <lmromero@anderson.edu>
2017-05-16 11:26:55 -0400

    update from instructor

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 const personForm = document.querySelector('form')
 
-const renderColor =(hairColor)=>{ 
-    const colorDiv document.createElement('div')
-
-    colorDiv.style.height=50px
-     colorDiv.style.weight=100px
-      colorDiv.style.backgroundColor= hairColor
-
-
-  return colorDiv
-}
-
-const renderListItem = (name,value) => {
-const li= document.createElement('li')
-li.innerHTML=`${name}:${value}`
-return li
-}
-
-const renderList = () => {
-    const list =document.createElement('ul')
-  Object.keys(person).map((fieldName,_i,_keys)=>{
-        let li= renderListItem(fieldName,person[fieldName])
-        list.appendChild(li)
-    })
-    
-    return list 
-}
-
 const handleSubmit = (ev) => {
   ev.preventDefault()
   const form = ev.target
   const details = document.querySelector('.details')
 
-const person={
-    name: form.personName.value,
-    hairColor: renderColor(form.hairColor.value).outerHTML,
-    age: form.age.value,
-    birthplace: form.birthplace.value,
-}
-    
-const list=renderList(person)
-details.appendChild(renderList())
-  
+  const personName = form.personName.value
+  const hairColor = form.hairColor.value
+  const age = form.age.value
+  const birthplace = form.birthplace.value
+
+  const colorDiv = `
+    <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>
+  `
+var list = document.createElement('ul')
+var text =document.createTextNode('Form Submition');
+ list.appendChild(text);
+ document.body.appendChild(list);
+
+var listItemName = document.createElement('li')
+var name =document.createTextNode(personName);
+ listItemName.appendChild(name);
+ document.body.appendChild(name);
+
+var listItemColor = document.createElement('li')
+var color = document.createTextNode(hairColor);
+ listItemColor.appendChild(color);
+ document.body.appendChild(listItemColor);
+
+ var listItemAge = document.createElement('li')
+var listAge =document.createTextNode(age);
+ listItemAge.appendChild(listAge);
+ document.body.appendChild(listItemAge);
+
+ var listItemBirthplace = document.createElement('li')
+var bplace =document.createTextNode(birthplace);
+ listItemBirthplace.appendChild(bplace);
+document.body.appendChild(listItemBirthplace);
+//   details.innerHTML = `
+    // <ul>
+    //   <li>Name: ${personName}</li>
+    //   <li>Hair Color: ${colorDiv}</li>
+    //   <li>Age: ${age}</li>
+    //   <li>Birthplace: ${birthplace}</li>
+    // </ul>
+//   `
 }
 
 personForm.addEventListener('submit', handleSubmit)
-
